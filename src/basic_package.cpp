b090d22a2196feeca82c93d1306c58fd0cdf3062
Asier Fernandez <asier.fernandez@tecnalia.com>
2018-06-22 13:08:55 +0200

    Rebase from 'debian/kinetic/basic_package'

--- src/basic_package.cpp
+++ src/basic_package.cpp
@@ -3,5 +3,6 @@ int main(int argc, char* argv[])
 {
   ros::init(argc, argv, "basic_package_node");
   ROS_INFO("basic_package_node finished succsessfully");
+  ROS_INFO("using version 0.0.2");
   return -1;
 }
